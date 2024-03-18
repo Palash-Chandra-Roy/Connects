2f46ce01f3d2fe242dafdb9e45fc49e3467bef19
pm <palashtpi21@gmail.com>
2024-03-19 01:07:28 +0600

    12 Commit

--- lib/Screen/Profile Screen/profile_screen.dart
+++ lib/Screen/Profile Screen/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:connect/Widgets/main_button.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -55,7 +56,7 @@ class ProfileScreen extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                    padding: EdgeInsets.only(left: 20, right: 20, top: 40),
+                    padding: EdgeInsets.only(left: 20, right: 20, top: 20),
                     child: Stack(
                       alignment: Alignment.bottomCenter,
                       clipBehavior: Clip.none,
@@ -67,24 +68,117 @@ class ProfileScreen extends StatelessWidget {
                               borderRadius:
                                   BorderRadius.all(Radius.circular(20)),
                               color: Colors.white),
-                            child: Column(
-                                children: [
-                                  Text("Palash")
-                                ],
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 40,
                               ),
+                              Text(
+                                "Palash Roy",
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.black),
+                              ),
+                              Text(
+                                "Flutter Developer",
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.black12),
+                              ),
+                              SizedBox(
+                                height: 100,
+                              ),
+                              Container(
+                                height: 40,
+                                width: 180,
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
+                                  color: Colors.red,
+                                ),
+                                child: Center(
+                                    child: Text(
+                                  "এডিট প্রোফাইল",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 14),
+                                )),
+                              )
+                            ],
+                          ),
                         ),
                         Positioned(
-                            top: -20,
+                          top: -20,
+                          child: CircleAvatar(
+                            radius: 25,
+                            backgroundImage: AssetImage(
+                              "assets/image/palash.jpg",
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                            top: 15,
+                            left: 170,
                             child: CircleAvatar(
-                              radius: 20,
-                             
-                              backgroundImage: AssetImage(
-                                "assets/image/palash.jpg",
-                              ),
+                                backgroundColor: Colors.grey,
+                                radius: 8,
+                                child: Icon(
+                                  Icons.camera_alt,
+                                  size: 10,
+                                  color: Colors.white,
+                                ))),
+                        Positioned(
+                            top: 90,
+                            left: 10,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.phone_android,
+                                      color: Colors.grey,
+                                    ),
+                                    Text(
+                                      "+8801826344872",
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 10),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.telegram,
+                                      color: Colors.grey,
+                                    ),
+                                    Text(
+                                      "palashtpi21@gmail.com",
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 10),
+                                    )
+                                  ],
+                                ),
+                              ],
                             )),
-                            
                       ],
                     )),
+                    
+              Padding(
+                padding: const EdgeInsets.only(top: 10,right: 20,left: 20),
+                child: Card(
+                  child: Container(
+                    height: size.height *0.2,
+                    width: size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white,
+                      
+                    ),
+                    child: Text("data"),
+                  ),
+                ),
+              )
               ],
             ),
           ],
